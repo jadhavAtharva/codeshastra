6601b364b3aacebfd6550df4d500b413dc72dee0
shubs12 <jungharesj7@gmail.com>
2020-03-07 19:52:59 +0530

    initial front end

---
+++ client/src/components/home.vue
@@ -0,0 +1,97 @@
+<template>
+<div id="back">
+     <div>
+         <h2 style="margin-left:10%">Generate Ticket</h2>
+         <br>
+      <form  v-on:submit="onSubmit" >
+        <div class="col-md-4">
+        <label for="dest">Source</label>
+        <input type="text" class="form-control" id="src" v-model="src" required>
+        </div>
+        <br>
+        <div class="col-md-4">
+        <label for="dest">Destination</label>
+        <input type="text" class="form-control" id="dest" v-model="dest" required>
+        </div>
+        <br>
+        <div class="col-md-4">
+          <label for="dest">Mode</label>
+        <select id="mode" class="form-control" v-model="mode">
+        <option selected value="single">Single</option>
+        <option value="return">Return</option>
+      </select>
+        </div>
+        <br>
+  <br>
+  <br>
+  <div>
+  <button type="submit" class="btn btn-primary" id="btn" style="margin-left:5%">Generate Ticket</button>
+  </div>
+
+      </form>
+    </div>
+    </div>
+
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+  name: 'App',
+  components: {
+  },
+  data () {
+    return {
+      mode: '',
+      dest:'',
+      src:''
+    }
+  },
+  computed: {
+    ...mapGetters(['user'])
+  },
+  methods: {
+    ...mapActions(['generate']),
+    onSubmit (e) {
+      e.preventDefault()
+      const generate = {
+        a: this.src,
+        b: this.dest,
+        f: this.mode
+      }
+      this.generate(generate)
+    }
+  }
+}
+</script>
+
+<style>
+#back{
+
+  background-repeat: no-repeat;
+  background-size: contain;
+  height: 700px;
+}
+#app {
+  font-family:'Times New Roman', Times, serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
+  font-size: 1.5rem;
+  line-height: 0.8;
+  letter-spacing: -0.005em;
+}
+h2 {
+  color: black;
+  font-weight: bold;
+  letter-spacing: -0.005em;
+}
+label {
+  text-align: center;
+  color: black;
+  font-weight: bold;
+}
+</style>
+
---
+++ client/src/components/navbar.vue
@@ -0,0 +1,65 @@
+<template>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand">Rail Yatri</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <router-link to="/home">
+        <a class="nav-link"> Home </a>
+        </router-link>
+      </li>
+      <li class="nav-item">
+        <router-link to="/">
+        <a class="nav-link"> Login </a>
+        </router-link>
+      </li>
+      <li class="nav-item">
+        <router-link to="/signup">
+        <a class="nav-link"> Signup </a>
+        </router-link>
+      </li>
+    </ul>
+  </div>
+</nav>
+</template>
+<script>
+import { mapGetters, mapActions } from 'vuex'
+import router from '../router'
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family:'Times New Roman', Times, serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: aliceblue;
+}
+
+#nav{
+  font-family:sans-serif;
+}
+.nav-link{
+  color: aliceblue;
+  text-decoration: none;
+}
+
+.navbar {
+font-size: 20px;
+line-height: 35px;
+letter-spacing: 0.019em;
+font-weight: 700;
+font-style: normal;
+}
+
+</style>
